6a20dd37c0cbe334d97750a734ed3c9a3974c82b
Gabriel Pinheiro <gabriel.b.pinheiro@hotmail.com>
2026-04-04 16:31:55 -0300

    feat: test checkin page

--- pages/2_checkin.py
+++ pages/2_checkin.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import streamlit as st
+
+from database import Database
+
+class CheckIn:
+    def __init__(self, database: Database):
+        self.database = database
+
+    def checkin_service(self):
+        st.set_page_config(page_title="Check-in Vôlei", page_icon="🏐")
