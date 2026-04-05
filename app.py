ff9de9c35f22383408b054b79b286d3a4be5baaa
Gabriel Pinheiro <gabriel.b.pinheiro@hotmail.com>
2026-04-04 22:34:29 -0300

    feat: add new user button

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import streamlit as st
 try:
     st.title("Bem-vindo ao Sistema de Vôlei")
 
+    if st.button("Cadastrar novo membro ✏", use_container_width=True, type="primary"):
+        # O caminho deve ser o nome do arquivo dentro da pasta pages/
+        # ou o nome do arquivo principal se for voltar para o início
+        st.switch_page("pages/1_register_user.py")
     # Criando um botão grande para o celular
     if st.button("Ir para o Check-in 🏐", use_container_width=True, type="primary"):
         # O caminho deve ser o nome do arquivo dentro da pasta pages/
